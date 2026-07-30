712c9d78943fca3b30594abafa4a9331ea78149a
Shreyas Samal <shreyassamal05@gmail.com>
2026-07-30 14:48:43 -0500

    first commit

--- intervals/script.js
+++ intervals/script.js
@@ -3,3 +3,9 @@ let time = 0;
 // reference to the <span> containing the number
 let counterElement = document.getElementById("time");
 
+function timer() {
+    time += 1
+    counterElement.innerHTML = time
+}
+
+setInterval(timer, 1000);
